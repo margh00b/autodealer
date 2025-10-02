5ba18b7ce8a089179333ec77f11d425cbca8cb11
Marghoob Ahmad <marghoobahmd25@gmail.com>
2025-10-02 16:09:11 -0600

    Fix#3

--- src/app/forms/financing/page.tsx
+++ src/app/forms/financing/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import FinancingForm from "@/components/FinancingForm/FinancingForm";
+import dynamic from "next/dynamic";
+
+const FinancingForm = dynamic(
+  () => import("@/components/FinancingForm/FinancingForm"),
+  { ssr: false }
+);
 
 export default function FinancingFormPage() {
   return <FinancingForm />;
